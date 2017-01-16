c14651d273ba7230c9b172582e86a2278f6018ab
Noots41 <z1pp@mail.ru>
2017-01-16 11:37:55 +0400

    Первая версия

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using C = Calc;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cals = new C.Calc();
+            int result = cals.Sum(1, 2);
+            Console.WriteLine($"result = {result}");
+            //Console.WriteLine(string.Format("result = {0}",result));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using C = Calc;
+
+namespace UnitTestProject1
+{
+    /// <summary>
+    /// Тестирование Calc
+    /// </summary>
+    [TestClass]
+    public class CalcUnitTest1
+    {
+        [TestMethod]
+        public void SumTest()
+        {
+            var calc = new C.Calc();
+            var result = calc.Sum(1, 2);
+            Assert.AreEqual(result, 3);
+        }
+    }
+}
