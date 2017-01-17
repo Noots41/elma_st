bf6a4de84b2006ea410f3f411a212929cd12632c
Noots41 <z1pp@mail.ru>
2017-01-17 08:43:45 +0400

    Тест незначительно подкорректирован

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -14,7 +14,7 @@ namespace UnitTestProject1
         public void SumTest()
         {
             var calc = new C.Calc(new C.IOperation[] { new C.SumOperation() });
-            var result = (int)calc.Execute("Sum", new object[] { 1, 2, 3 });
+            var result = (int)calc.Execute("Sum", new object[] { 1, 2 });
             Assert.AreEqual(result, 3);
         }
 
@@ -30,7 +30,7 @@ namespace UnitTestProject1
         public void NullTest()
         {
             var calc = new C.Calc(new C.IOperation[] { new C.NullOperation() });
-            var result = (int)calc.Execute("Null", new object[] { 1, 2, 3 });
+            var result = (int)calc.Execute("Null", new object[] { });
             Assert.AreEqual(result, 0);
         }
 
@@ -38,7 +38,7 @@ namespace UnitTestProject1
         public void SingleTest()
         {
             var calc = new C.Calc(new C.IOperation[] { new C.SingleOperation() });
-            var result = (int)calc.Execute("Single", new object[] { 3, 4 });
+            var result = (int)calc.Execute("Single", new object[] { 3 });
             Assert.AreEqual(result, 9);
         }
     }
