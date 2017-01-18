dcb5f7cb68de9485bf23019304e2502d47d6d6c1
Noots41 <z1pp@mail.ru>
2017-01-18 09:07:33 +0400

    + label

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -38,6 +38,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.lblResult = new System.Windows.Forms.Label();
             this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -129,11 +130,21 @@
             this.textBox5.TabIndex = 5;
             this.textBox5.Text = "1";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(388, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Args";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(509, 261);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.lblResult);
             this.Controls.Add(this.button1);
@@ -162,6 +173,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label lblResult;
         private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label2;
     }
 }
 
