383b4c4c877878e70a43d9d70248eb1a903dcdbe
Noots41 <z1pp@mail.ru>
2017-01-20 08:51:44 +0400

    Старое задание + база

--- elmatest001-master/Web/Controllers/CalcController.cs
+++ elmatest001-master/Web/Controllers/CalcController.cs
@@ -5,30 +5,69 @@ using System.Web;
 using System.Web.Mvc;
 using Calc;
 using Web.Models;
+using System.Reflection;
+using System.IO;
+using System.Web.Hosting;
 
 namespace Web.Controllers
 {
     public class CalcController : Controller
     {
+        private Calc.Calc Calculator { get; set; }
+
+        public CalcController()
+        {
+            var operations = new List<IOperation>();
+
+            #region Получение всех возможных операций
+            // найти файлы dll и exe в текущей директории
+            var files = Directory.GetFiles(HostingEnvironment.MapPath("~/") + "\\App_Data", "*.dll");
+
+            //загрузить их
+            foreach (var file in files)
+            {
+                // Console.WriteLine(file);
+                var assembly = Assembly.LoadFile(file);
+
+                foreach (var type in assembly.GetTypes().Where(t => t.IsClass))
+                {
+                    // найти реализацюию интерфейса IOperation
+                    var interfaces = type.GetInterfaces();
+                    if (interfaces.Contains(typeof(IOperation)))
+                    {
+                        //создаем экземпляр класса и приводим к нужному интерфейсу
+                        var oper = Activator.CreateInstance(type) as IOperation;
+                        if (oper != null)
+                        {
+                            operations.Add(oper);
+                        }
+                    }
+                }
+            }
+            #endregion
+
+            Calculator = new Calc.Calc(operations);
+        }
         // GET: Calc
         public ActionResult Index()
         {
+            var opers = Calculator.GetOperationNames().Select(o => new SelectListItem() { Text = o, Value = o });
+            ViewBag.Operations = opers;
             return View(new OperationModel());
         }
 
         public ActionResult Execute(OperationModel model)
         {
-            
             if (!ModelState.IsValid)
             {
                 return View("Index", model);
             }
-            var calc = new Calc.Calc(new IOperation[] { new SumOperation() });
-            var result = calc.Execute(model.Name, model.GetParameters());
+
+            var result = Calculator.Execute(model.Name, model.GetParameters());
             ViewData.Model = $"result = {result}";
             return View();
         }
 
-        
+
     }
 }
